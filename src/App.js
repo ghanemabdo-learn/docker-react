12683707de1bbe113759b27ce0fe955041d9c1b5
Abdurrahman Ghanem <abdurrahmanghanem@Abdus-MacBook-Pro.local>
2020-09-06 22:27:06 -0400

    Change app.js text

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload. Hi there! I am good :) Happy
+          Edit <code>src/App.js</code> and show this feature
         </p>
         <a
           className="App-link"
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          I changed to feature branch
+          Learn React
         </a>
       </header>
     </div>
